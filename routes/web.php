212eaf870123991396a0babd968a6d46eaa3d0e1
ejieflorida128 <ejieflorida128@gmail.com>
2025-02-22 14:44:10 +0800

    [ejie] - changes in the index, login and register section
    changes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/login', function () {
+    return view('Auth.login');
+})->name('login');
+Route::get('/register', function () {
+    return view('Auth.register');
+})->name('register');
+
